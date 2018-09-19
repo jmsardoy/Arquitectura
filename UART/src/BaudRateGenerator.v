0bea07ba54c265a2963d0da5fb41dfabfd139ea7
Federico Perez <federico@eclypsium.com>
2018-09-19 17:34:52 -0300

    Baud rate generator first working version

--- UART/src/BaudRateGenerator.v
+++ UART/src/BaudRateGenerator.v
@@ -1,24 +1,27 @@
-`define FREQUENCY 100000000   // Frequency ay which we operate
-`define BAUD_RATE 9600		// Selected baud rate
+`define FREQUENCY 50000000   // Frequency ay which we operate
+`define BAUD_RATE 19200		// Selected baud rate
 
 module BaudRateGenerator
     #(parameter FREQUENCY = `FREQUENCY,
       parameter BAUD_RATE = `BAUD_RATE)
                 (input clk, 
                  input rst,
-                 output out);
+                 output reg out);
 
-	localparam MAX_COUNT = FREQUENCY / BAUD_RATE;
+	localparam MAX_COUNT = (FREQUENCY / (BAUD_RATE * 16)) + 1;
 
 	integer count = 0;
-	always@(posedge clk)
-	begin
-		count = count + 1;
-		if(count == MAX_COUNT)
-		begin
-			out = ~out;
-			count = 0;
-		end
+	always@(posedge clk or negedge rst)
+	begin 
+	    if (!rst) out = 0;
+        else begin
+            count = count + 1;
+            if(count == MAX_COUNT)
+            begin
+                out = ~out;
+                count = 0;
+            end
+	    end
 	end
 
 endmodule
