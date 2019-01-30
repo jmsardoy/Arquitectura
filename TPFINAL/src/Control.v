a7b3c44eb4896ad933792c7f5183b520f1f46c49
Juan Manuel Sardoy <jmsardoy@gmail.com>
2019-01-30 16:03:20 -0300

    implemented mux inside Control Unit

--- TPFINAL/src/Control.v
+++ TPFINAL/src/Control.v
@@ -8,6 +8,7 @@ module Control
 )
 (
     input wire  [OPCODE_BITS - 1 : 0] i_opcode,
+    input wire                        i_control_mux,
     output reg                        o_RegDst,
     output reg  [1:0]                 o_ALUOp,
     output reg                        o_ALUSrc,
@@ -24,51 +25,63 @@ module Control
 
     //Truth table figure 4.22 or table figure 4.49
     always@* begin
-        case (i_opcode)
-            RFORMAT: begin
-                o_RegDst   <= 1;
-                o_ALUSrc   <= 0;
-                o_MemtoReg <= 0;
-                o_RegWrite <= 1;
-                o_MemRead  <= 0;
-                o_MemWrite <= 0;
-                o_Branch   <= 0;
-                o_ALUOp    <= 'b10; 
-            end
+        if (i_control_mux) begin
+            case (i_opcode)
+                RFORMAT: begin
+                    o_RegDst   <= 1;
+                    o_ALUSrc   <= 0;
+                    o_MemtoReg <= 0;
+                    o_RegWrite <= 1;
+                    o_MemRead  <= 0;
+                    o_MemWrite <= 0;
+                    o_Branch   <= 0;
+                    o_ALUOp    <= 'b10; 
+                end
 
-            LW: begin
-                o_RegDst   <= 0;
-                o_ALUSrc   <= 1;
-                o_MemtoReg <= 1;
-                o_RegWrite <= 1;
-                o_MemRead  <= 1;
-                o_MemWrite <= 0;
-                o_Branch   <= 0;
-                o_ALUOp    <= 'b00; 
-            end
+                LW: begin
+                    o_RegDst   <= 0;
+                    o_ALUSrc   <= 1;
+                    o_MemtoReg <= 1;
+                    o_RegWrite <= 1;
+                    o_MemRead  <= 1;
+                    o_MemWrite <= 0;
+                    o_Branch   <= 0;
+                    o_ALUOp    <= 'b00; 
+                end
 
-            SW: begin
-                o_RegDst   <= 0; //x
-                o_ALUSrc   <= 1;
-                o_MemtoReg <= 0; //x
-                o_RegWrite <= 0; 
-                o_MemRead  <= 0;
-                o_MemWrite <= 1;
-                o_Branch   <= 0;
-                o_ALUOp    <= 'b00; 
-            end
+                SW: begin
+                    o_RegDst   <= 0; //x
+                    o_ALUSrc   <= 1;
+                    o_MemtoReg <= 0; //x
+                    o_RegWrite <= 0; 
+                    o_MemRead  <= 0;
+                    o_MemWrite <= 1;
+                    o_Branch   <= 0;
+                    o_ALUOp    <= 'b00; 
+                end
 
-            BEQ: begin
-                o_RegDst   <= 0; //x
-                o_ALUSrc   <= 0;
-                o_MemtoReg <= 0; //x
-                o_RegWrite <= 0;
-                o_MemRead  <= 0;
-                o_MemWrite <= 0;
-                o_Branch   <= 1;
-                o_ALUOp    <= 'b01; 
-            end
-        endcase
+                BEQ: begin
+                    o_RegDst   <= 0; //x
+                    o_ALUSrc   <= 0;
+                    o_MemtoReg <= 0; //x
+                    o_RegWrite <= 0;
+                    o_MemRead  <= 0;
+                    o_MemWrite <= 0;
+                    o_Branch   <= 1;
+                    o_ALUOp    <= 'b01; 
+                end
+            endcase
+        end
+        else begin
+            o_RegDst   <= 0;
+            o_ALUSrc   <= 0;
+            o_MemtoReg <= 0;
+            o_RegWrite <= 0;
+            o_MemRead  <= 0;
+            o_MemWrite <= 0;
+            o_Branch   <= 0;
+            o_ALUOp    <= 'b00; 
+        end
     end
 
 endmodule
--- TPFINAL/tb/tb_Control.v
+++ TPFINAL/tb/tb_Control.v
@@ -4,6 +4,7 @@
 module tb_Control();
 
     reg  [5:0] opcode;
+    reg        control_mux;
     wire       RegDst;
     wire [1:0] ALUOp;
     wire       ALUSrc;
@@ -19,6 +20,7 @@ module tb_Control();
     localparam BEQ     = 'b000100;
 
     initial begin
+        control_mux = 1;
         opcode = RFORMAT;
         #20
         opcode = LW;
@@ -26,10 +28,15 @@ module tb_Control();
         opcode = SW;
         #20
         opcode = BEQ;
+
+        //testing control_mux
+        #20
+        control_mux = 0;
     end
 
     Control Control_u(
         .i_opcode(opcode),
+        .i_control_mux(control_mux),
         .o_RegDst(RegDst),
         .o_ALUOp(ALUOp),
         .o_ALUSrc(ALUSrc),
