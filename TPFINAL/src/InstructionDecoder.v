e48d39c611c6618ae20d615d1d7f653e606242ff
Juan Manuel Sardoy <jmsardoy@gmail.com>
2019-02-18 17:30:38 -0300

    finished instructionDecoder

--- TPFINAL/src/InstructionDecoder.v
+++ TPFINAL/src/InstructionDecoder.v
@@ -21,6 +21,7 @@ module InstructionDecoder
     input wire                            i_mem_wb_RegWrite,
 
 
+
     //HazardDetector input
     input wire [REG_ADDRS_BITS - 1 : 0] i_id_ex_rt,
     input wire                          i_id_ex_MemRead,
@@ -32,13 +33,14 @@ module InstructionDecoder
 
     //control signals outputs
     output wire       o_RegDst,
-    output wire [1:0] o_ALUOp,
-    output wire       o_ALUSrc,
-    output wire       o_Branch,
+    output wire       o_RegWrite,
     output wire       o_MemRead,
     output wire       o_MemWrite,
-    output wire       o_RegWrite,
     output wire       o_MemtoReg,
+    output wire [3:0] o_ALUOp,
+    output wire       o_ALUSrc,
+    output wire       o_Shamt,
+    output wire       o_ls_filter_op,
 
     //RegisterFile/Data output
     output wire [PROC_BITS - 1 : 0] o_read_data_1,
@@ -50,6 +52,10 @@ module InstructionDecoder
     output wire [REG_ADDRS_BITS - 1 : 0] o_rt,
     output wire [REG_ADDRS_BITS - 1 : 0] o_rd,
 
+    //Other outputs
+    output wire [PC_BITS - 1 : 0]    o_PCNext,
+    output wire [OPCODE_BITS - 1 :0] o_opcode,
+    output wire [PC_BITS - 1 : 0]    o_jump_address,
 
     //Debug output
     output wire [32*PROC_BITS - 1 : 0] o_rf_regs
@@ -63,6 +69,11 @@ module InstructionDecoder
     wire [REG_ADDRS_BITS - 1 : 0] rt             = i_instruction[20:16];
     wire [REG_ADDRS_BITS - 1 : 0] rd             = i_instruction[15:11];
     wire [15 : 0]                 immediate_data = i_instruction[15:0];
+    wire [25 : 0]                 jump_immediate = i_instruction[25:0];
+    wire [5:0]                    funct          = i_instruction[5:0];
+
+    assign o_PCNext = i_PCNext;
+    assign o_opcode = opcode;
 
 
     //control mux from hazard to control unit
@@ -70,6 +81,8 @@ module InstructionDecoder
 
     //sign extend
     assign o_immediate_data_ext = {{16{immediate_data[15]}}, immediate_data};
+    assign o_jump_address       = {i_PCNext[31:26], jump_immediate};
+
 
     //Reg src and dest outputs
     assign o_rs = rs;
@@ -91,15 +104,17 @@ module InstructionDecoder
 
     Control Control_u(
         .i_opcode(opcode),
+        .i_funct(funct),
         .i_control_mux(control_mux),
         .o_RegDst(o_RegDst),
-        .o_ALUOp(o_ALUOp),
-        .o_ALUSrc(o_ALUSrc),
-        .o_Branch(o_Branch),
+        .o_RegWrite(o_RegWrite),
         .o_MemRead(o_MemRead),
         .o_MemWrite(o_MemWrite),
-        .o_RegWrite(o_RegWrite),
-        .o_MemtoReg(o_MemtoReg)
+        .o_MemtoReg(o_MemtoReg),
+        .o_ALUOp(o_ALUOp),
+        .o_ALUSrc(o_ALUSrc),
+        .o_Shamt(o_Shamt),
+        .o_ls_filter_op(o_ls_filter_op)
     );
 
     HazardDetector HazardDetector_u(
